bf029843eda0d90e44c9248b4f9b2a556ae2d280
sumeshvasu <sumeshvasu@gmail.com>
2024-02-27 01:49:10 +0530

    almost done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -861,10 +861,12 @@
             </div>
 
             <div class="flex justify-center mt-6">
-                <div
-                    class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-                    <h1>Online Exam Portal</h1>
-                </div>
+                <a href="{{ url('/exam') }}">
+                    <div
+                        class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <h1>Online Exam Portal</h1>
+                    </div>
+                </a>
             </div>
 
             <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
